6fc16f2349cd7b252d21e5422a89bc1f4188ff80
Anatoliy Korovin <antkorwin@gmail.com>
2018-06-21 09:36:38 +1000

    transactional wrapper

--- src/main/java/com/antkorwin/statemachineutils/wrapper/StateMachineWrapper.java
+++ src/main/java/com/antkorwin/statemachineutils/wrapper/StateMachineWrapper.java
@@ -7,10 +7,12 @@ import java.util.function.Consumer;
 /**
  * Created on 04.06.2018.
  *
+ * StateMachineWrapper interface
+ *
  * @author Korovin Anatoliy
  */
 public interface StateMachineWrapper<StatesT, EventsT> {
 
-    void runWithRollback(StateMachine<StatesT, EventsT> machine,
-                         Consumer<StateMachine<StatesT, EventsT>> runnable);
+    void runWithRollback(StateMachine<StatesT, EventsT> stateMachine,
+                         Consumer<StateMachine<StatesT, EventsT>> processingFunction);
 }
--- src/main/java/com/antkorwin/statemachineutils/wrapper/WrapperConfig.java
+++ src/main/java/com/antkorwin/statemachineutils/wrapper/WrapperConfig.java
@@ -1,6 +1,9 @@
 package com.antkorwin.statemachineutils.wrapper;
 
+import com.antkorwin.statemachineutils.wrapper.impl.StateMachineRollbackWrapper;
+import com.antkorwin.statemachineutils.wrapper.impl.StateMachineTransactionalWrapper;
 import com.antkorwin.xsync.XSync;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,5 +25,13 @@ public class WrapperConfig {
     @Bean("stateMachineRollbackWrapper")
     public StateMachineWrapper stateMachineWrapper(XSync<UUID> stateMachineXSync) {
         return new StateMachineRollbackWrapper<>(stateMachineXSync);
+    }
+
+    @Bean("stateMachineTransactionalWrapper")
+    public <StatesT, EventsT> StateMachineWrapper stateMachineTransactionalWrapper(
+            @Qualifier("stateMachineRollbackWrapper")
+                    StateMachineWrapper<StatesT, EventsT> stateMachineRollbackWrapper) {
+
+        return new StateMachineTransactionalWrapper<>(stateMachineRollbackWrapper);
     }
 }
